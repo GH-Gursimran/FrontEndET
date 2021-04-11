12a338df9a6f2f04f809f5d527b9923c961784ed
GH-Gursirman <sohanpalgursimransingh@gmail.com>
2021-04-11 16:35:30 +0530

    minor changes and alert message added

--- javascript/script.js
+++ javascript/script.js
@@ -10,18 +10,32 @@ document.getElementById("submit-button").addEventListener("click", function(even
     if(country!="" && from_date!="" && to_date!=""){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
-            if(this.readyState == 4){
+            if(this.readyState == 4 && this.status == 200){
                 var myarr = JSON.parse(this.responseText);
-                console.log(myarr);
-                console.log(this.status);
+                console.log("Status: "+this.status);
                 printFunction(myarr)
             }
         } 
         var api_url = "https://api.covid19api.com/country/" + country + "?from=" + from_date + "T00:00:00Z&to="+to_date+"T00:00:00Z"
         xhttp.open("GET",api_url);
-        xhttp.send();
-        
+        xhttp.send();    
     } 
+    else {
+        var message = "Please check input again.\n"
+        var country = document.getElementById("country").value;
+        var from_date = document.getElementById("from_date").value;
+        var to_date = document.getElementById("to_date").value;
+        if(country===""){
+            message += "\n Country not specified";
+        }
+        if(from_date===""){
+            message += "\n Starting Date not specified";
+        }
+        if(to_date===""){
+            message += "\n End Date not specified";
+        }
+        alert(message);
+    }
 });
 
 function printFunction(myarr){
